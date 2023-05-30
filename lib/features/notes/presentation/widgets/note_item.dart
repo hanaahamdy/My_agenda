bedd6a81ef391a352d6a8b863684bbd21992b377
hanaahamdy <hanaahamdy514@gmail.com>
2023-05-30 18:51:36 +0300

    Add Edit Screen

--- lib/features/notes/presentation/widgets/note_item.dart
+++ lib/features/notes/presentation/widgets/note_item.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_agenda/features/EditNote/presentaion/Widgets/editView.dart';
 import 'package:my_agenda/features/notes/presentation/Cubit/noteCubit.dart';
 
 import '../../../../core/utils/colors.dart';
+import '../../../EditNote/presentaion/editViewBody.dart';
 import '../../data/NoteModel.dart';
 
 class NoteItem extends StatelessWidget {
@@ -12,36 +14,42 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * .02),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.teal,
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(
-              note.title,
-              style: const TextStyle(fontSize: 22),
+    return GestureDetector(onTap: (){
+Navigator.push(context, MaterialPageRoute(builder: (context){
+  return EditScreen(noteModel: note,);
+}));
+    },
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * .02),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.teal,
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                note.title,
+                style: const TextStyle(fontSize: 22),
+              ),
+              subtitle: Text(
+                note.subtitle,
+                style: TextStyle(color: black.withOpacity(.4), fontSize: 18),
+              ),
+              trailing: IconButton(
+                  onPressed: () {
+                    note.delete();
+                    BlocProvider.of<NoteCubit>(context).fetchNotes();
+                  },
+                  icon: const Icon(Icons.delete)),
             ),
-            subtitle: Text(
-              note.subtitle,
-              style: TextStyle(color: black.withOpacity(.4), fontSize: 18),
-            ),
-            trailing: IconButton(
-                onPressed: () {
-                  note.delete();
-                  BlocProvider.of<NoteCubit>(context).fetchNotes();
-                },
-                icon: const Icon(Icons.delete)),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [Text(DateTime.now().toString().split(" ").first)],
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Text(DateTime.now().toString().split(" ").first)],
+            )
+          ],
+        ),
       ),
     );
   }
